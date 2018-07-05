ecbbd2dfd6bc5e4178daf7c73d75abd0af4c7c30
Matt <matthew.j.wright@charter.com>
2018-07-05 16:48:41 +0000

    Slight change

--- myApp.py
+++ myApp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_whale():
-    return 'Whale, hello there!\n'
+    return 'Hello, you blubbery blob!\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
